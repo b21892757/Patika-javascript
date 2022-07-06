019b66037e07653ad316a005788da14cf068de36
İbrahim Enes Genişyürek <72977328+b21892757@users.noreply.github.com>
2022-07-06 09:01:09 +0300

    Create js-clock.js

---
+++ js-hw1/js-clock.js
@@ -0,0 +1,22 @@
+      let name = prompt('Lütfen İsminizi Giriniz ')
+			let added = document.querySelector("#myName")
+			added.innerHTML = name;
+      function showTime() {
+        const today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s;
+        setTimeout(showTime, 1000);
+        
+      }
+      
+      function checkTime(i) {
+        if (i < 10) {
+        i = "0" + i
+        }
+        return i;
+      }
+      showTime()
